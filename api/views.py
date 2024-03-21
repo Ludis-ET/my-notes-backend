682e25a8dd61ef0c3dc535d00c11ab807523dfc3
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-21 10:49:55 +0300

    django rest framework

--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.views import Response
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(["GET"])
 def getRoutes(request):
     routes = [
         {
@@ -35,4 +37,4 @@ def getRoutes(request):
             'description': 'Deletes and exiting note'
         },
     ]
-    return JsonResponse(routes,safe=False)
+    return Response(routes)
--- notes/settings.py
+++ notes/settings.py
@@ -30,7 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'api',
-    'rest_framework'
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
