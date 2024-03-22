77c1096bae2abe5ee1d9283bb239281a45465315
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-22 15:52:26 +0300

    adding postgress db

--- notes/settings.py
+++ notes/settings.py
@@ -78,6 +78,19 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': os.getenv('PGDATABASE'),
+    'USER': os.getenv('PGUSER'),
+    'PASSWORD': os.getenv('PGPASSWORD'),
+    'HOST': os.getenv('PGHOST'),
+    'PORT': os.getenv('PGPORT', 5432),
+    'OPTIONS': {
+      'sslmode': 'require',
+    },
+  }
+}
 
 
 # Password validation
