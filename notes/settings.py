27aa4e7f385913317fe1afd29f16da777e6a712b
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-22 15:44:10 +0300

    Change STATIC\_URL from "/static/" to "/staticfiles/"; update STATICFILES\_STORAGE to use Whitenoise's CompressedManifestStaticFilesStorage
    This diff changes the STATIC\_URL setting from "/static/" to "/staticfiles/". It also updates the STATICFILES\_STORAGE setting to use Whitenoise's CompressedManifestStaticFilesStorage. This is done to improve the handling of static files in a Django project.

--- notes/settings.py
+++ notes/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
