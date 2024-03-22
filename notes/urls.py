0d38f9010ea7b0beeab889b9a42cfe49b022f8f8
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-22 14:02:30 +0300

    "Added static URLs to urlpatterns if DEBUG is True; no other changes"
    Explanation:
    
    -The diff shows that a line of code was moved out of an if statement in the urls.py file of the 'notes' directory.
    -The line of code in question is "urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)".
    -This line of code is used to serve static files during development, when the DEBUG setting is set to True.
    -By moving this line of code outside of the if statement, it will always be added to the urlpatterns list, regardless of the value of DEBUG.
    -However, since the code was already present in the file, the only change is the removal of the if statement, and not the addition of any new functionality.

--- notes/urls.py
+++ notes/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('', include('api.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
