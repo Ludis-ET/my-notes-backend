5a7c98769a2ed263953f415f02ea1fb513658c8c
Ludis-et <leulsegedmelaku1020@gmail.com>
2024-03-22 13:08:07 +0300

    "Update WSGI file and add requirements for CORS headers and Django REST framework"
    This commit updates the `wsgi.py` file to include a new variable `app` that points to the WSGI callable. It also adds the requirements for `django-cors-headers` and `djangorestframework` to the `requirements.txt` file.

--- notes/wsgi.py
+++ notes/wsgi.py
@@ -1,12 +1,3 @@
-"""
-WSGI config for notes project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +5,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'notes.settings')
 
 application = get_wsgi_application()
+
+app = application
